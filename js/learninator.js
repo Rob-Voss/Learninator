12ea2ac4021bdbc7ab1d477a1bdc3f51f6e1664a
Rob Voss <rob.voss@digerati.io>
2015-03-30 16:09:32 -0700

    Merge commit '07795f721ab72e72fedd64e7de69e81f2595f93b' into HEAD
    Conflicts:
    	index.html
    	js/entities/Agent.js
    	js/entities/Wall.js
    	js/entities/World.js
    	js/environments/Maze.js
    	js/learninator.js
    	js/lib/deepqlearn.js
    	js/lib/util.js

--- js/learninator.js
+++ js/learninator.js
@@ -326,3 +326,108 @@ function start() {
 
 	go('mid');
 }
+
+/**
+ * Line intersection helper function: line segment (v1,v2) intersect segment (v3,v4)
+ * @param {Vec} v1
+ * @param {Vec} v2
+ * @param {Vec} v3
+ * @param {Vec} v4
+ * @returns {lineIntersect.learninatorAnonym$0|Boolean}
+ */
+var lineIntersect = function (v1, v2, v3, v4) {
+	// Line 1: 1st Point
+	var x1 = v1.x,
+			y1 = v1.y,
+			// Line 1: 2nd Point
+			x2 = v2.x,
+			y2 = v2.y,
+			// Line 2: 1st Point
+			x3 = v3.x,
+			y3 = v3.y,
+			// Line 2: 2nd Point
+			x4 = v4.x,
+			y4 = v4.y,
+			denom = (y4 - y3) * (x2 - x1) - (x4 - x3) * (y2 - y1);
+
+	if (denom === 0.0) {
+		// Parallel lines if it be this yar!
+		return false;
+	}
+
+	var pX = ((x4 - x3) * (y1 - y3) - (y4 - y3) * (x1 - x3)) / denom,
+			pY = ((x2 - x1) * (y1 - y3) - (y2 - y1) * (x1 - x3)) / denom;
+
+	if (pX > 0.0 && pX < 1.0 && pY > 0.0 && pY < 1.0) {
+		// Intersection point
+		var vecI = new Vec(x1 + pX * (x2 - x1), y1 + pX * (y2 - y1));
+
+		return {
+			vecX: pX,
+			vecY: pY,
+			vecI: vecI
+		};
+	}
+	return false;
+};
+
+/**
+ * Find the position of intersect between a line and a point
+ * @param {Vec} v1
+ * @param {Vec} v2
+ * @param {Vec} v0
+ * @param {Number} rad
+ * @returns {linePointIntersect.learninatorAnonym$1|Boolean}
+ */
+var linePointIntersect = function (v1, v2, v0, rad) {
+	// Create a perpendicular vector
+	var x = v2.y - v1.y,
+			y = v2.x - v1.x,
+			xDiff = v1.y - v0.y,
+			yDiff = v1.x - v0.x,
+			v = new Vec(x, -y),
+			d = Math.abs(y * xDiff - yDiff * x),
+			vecX = 0;
+
+	d = d / v.length();
+	if (d > rad) {
+		return false;
+	}
+
+	v.normalize();
+	v.scale(d);
+
+	var vecI = v0.add(v);
+	if (Math.abs(y) > Math.abs(x)) {
+		vecX = (vecI.x - v1.x) / (y);
+	} else {
+		vecX = (vecI.y - v1.y) / (x);
+	}
+	if (vecX > 0.0 && vecX < 1.0) {
+		return {
+			vecX: vecX,
+			vecI: vecI
+		};
+	}
+	return false;
+};
+
+/**
+ * World object contains many agents and walls and food and stuff
+ * @param {List} lst
+ * @param {Number} x
+ * @param {Number} y
+ * @param {Number} w
+ * @param {Number} h
+ * @returns {List}
+ */
+var utilAddBox = function (lst, x, y, w, h) {
+	var xw = x + w,
+			yh = y + h;
+	lst.push(new Wall(new Vec(x, y), new Vec(xw, y)));
+	lst.push(new Wall(new Vec(xw, y), new Vec(xw, yh)));
+	lst.push(new Wall(new Vec(xw, yh), new Vec(x, yh)));
+	lst.push(new Wall(new Vec(x, yh), new Vec(x, y)));
+
+	return lst;
+};
